a21372c86215637b367363c8b2616d1d40a3b69b
WK WK <witold.kepczynski@gmail.com>
2023-05-06 18:17:09 +0200

    wip

--- DAW.Repositories/ClipsRepository.cs
+++ DAW.Repositories/ClipsRepository.cs
@@ -18,8 +18,6 @@ public class ClipsRepository
         _notificationPublisher = notificationPublisher;
     }
 
-    // TODO Clip should have some Null Object?
-    // TODO this might be mapper?
     // TODO TEST IT!!!
     public async Task<Clip?> Get(Guid id)
     {
@@ -27,15 +25,7 @@ public class ClipsRepository
 
         if (clipState == null) return null;
 
-        var soundsModels = new List<EventAtLocation<SoundEvent>>();
-        foreach (var sound in clipState.Sounds)
-        {
-            var locationModel = Location.Create(sound!.Location!.Start);
-            var soundEvent = SoundEvent.Create(sound!.Event!.Velocity, sound.Event.Pitch, sound.Event.Length, sound.Event.Offset);
-            soundsModels.Add(EventAtLocation<SoundEvent>.Create(locationModel, soundEvent));
-        }
-
-        var clipModel = Clip.Create(soundsModels.ToArray(), clipState.Length, clipState.SourceId, NullNotificationPublisher.Instance);
+        var clipModel = clipState.ToModel();
 
         clipModel.Activate(_notificationPublisher);
 
---
+++ DAW.Repositories/States/StateExtensions.cs
@@ -0,0 +1,32 @@
+﻿using DAW.DDD.Domain.Entities;
+using DAW.DDD.Domain.Notifications;
+using DAW.DDD.Domain.ValueObjects;
+using DAW.Repositories.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAW.Repositories.States;
+internal static class StateExtensions
+{
+    public static ICollection<EventAtLocation<SoundEvent>> ToModel(this ICollection<EventAtLocationState<SoundEventState>> soundStates)
+    {
+        var soundsModels = new List<EventAtLocation<SoundEvent>>();
+
+        foreach (var sound in soundStates)
+        {
+            var locationModel = Location.Create(sound!.Location!.Start);
+            var soundEvent = SoundEvent.Create(sound!.Event!.Velocity, sound.Event.Pitch, sound.Event.Length, sound.Event.Offset);
+            soundsModels.Add(EventAtLocation<SoundEvent>.Create(locationModel, soundEvent));
+        }
+
+        return soundsModels;
+    }
+
+    public static Clip ToModel(this ClipState clipState)
+    {
+        return Clip.Create(clipState.Sounds.ToModel(), clipState.Length, clipState.SourceId, NullNotificationPublisher.Instance);
+    }
+}
