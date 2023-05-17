92c1b49aef024ad230b9048fc42718baddcf21ec
WK WK <witold.kepczynski@gmail.com>
2023-05-17 22:12:22 +0200

    clip test

--- DAW.Services.Tests/TrackServiceTests.cs
+++ DAW.Services.Tests/TrackServiceTests.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using DAW.DDD.Domain.Notifications;
+using DAW.DDD.Domain.Notifications.Clips;
 using DAW.DDD.Domain.Notifications.Tracks;
 using DAW.Repositories;
 using DAW.Repositories.DataAccess;
@@ -14,31 +15,67 @@ namespace DAW.Services.Tests;
 
 public class TrackServiceTests
 {
-    [Fact]
-    public void NewDefinedTrackServiceShouldBeRetrieved_Test()
+    private readonly TrackService _trackService;
+    private readonly INotificationPublisher _notificationPublisher;
+    private readonly IModelStateReader<ClipState> _clipStateProvider;
+    private readonly IModelStateReader<TrackState> _trackStateProvider;
+
+    public TrackServiceTests()
     {
-        //Arrange
-        var notificationProvider = Substitute.For<INotificationPublisher>();
-        var clipStateProvider = Substitute.For<IModelStateReader<ClipState>>();
-        var trackStateProvider = Substitute.For<IModelStateReader<TrackState>>();
+        _notificationPublisher = Substitute.For<INotificationPublisher>();
+        _clipStateProvider = Substitute.For<IModelStateReader<ClipState>>();
+        _trackStateProvider = Substitute.For<IModelStateReader<TrackState>>();
 
         var mapper = (new MapperConfiguration(cfg => cfg.AddMaps(typeof(Mappings)))).CreateMapper();
 
-        var repository = new ModelRepository(clipStateProvider, trackStateProvider, notificationProvider);
+        var repository = new ModelRepository(_clipStateProvider, _trackStateProvider, _notificationPublisher);
 
-        var sut = new TrackService(notificationProvider, mapper, repository);
+        _trackService = new TrackService(_notificationPublisher, mapper, repository);
+    }
 
-        var sourceId = (Guid.NewGuid());
+    [Fact]
+    public void NewDefinedTrackShouldCreateEventsAndObjects_Test()
+    {
+        //Arrange
+        var sourceId = Guid.NewGuid();
+
         //Act
-
-        var result = sut.CreateTrack(sourceId, "dummy");
+        var result = _trackService.CreateTrack(sourceId, "dummy");
 
         //Assert
-
-        notificationProvider.Received(1).Publish(Arg.Any<TrackCreatedNotification>());
-        notificationProvider.Received(1).Publish(Arg.Any<ChangeTrackSourceIdNotification>());
+        _notificationPublisher.Received(1).Publish(Arg.Any<TrackCreatedNotification>());
+        _notificationPublisher.Received(1).Publish(Arg.Any<ChangeTrackSourceIdNotification>());
 
         result.Id.Should().NotBeEmpty();
         result.SourceId.Should().Be(sourceId);
+    }
+
+    [Fact]
+    public async Task NewDefinedClipShouldCreateEventsAndObjects_Test()
+    {
+        // Arrange 
+        var trackLength = 1000;
+
+        var dummyTrack = new TrackState
+        {
+            Clips = new List<EventAtLocationState<Guid>>(),
+            Id = Guid.NewGuid(),
+            SourceId = Guid.NewGuid(),
+        };
+
+        _trackStateProvider.TryGet(Arg.Any<Guid>()).Returns(o => Task.FromResult((TrackState?)dummyTrack));
+
+        //Act
+
+        var clip = await _trackService.CreateClip(Guid.NewGuid(), trackLength);
+
+        //Assert
+        _notificationPublisher.Received(1).Publish(Arg.Any<ClipAddedToTrackNotification>());
+        _notificationPublisher.Received(1).Publish(Arg.Any<ChangeLengthOfClipNotification>());
+
+        clip.Should().NotBeNull();
+        clip!.Id.Should().NotBeEmpty();
+        clip.Length_ms.Should().Be(trackLength);
+
     }
 }
