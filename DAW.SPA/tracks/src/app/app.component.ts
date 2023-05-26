53923ed03ba67aae6a95937a4d8014f89cfa8204
WK WK <witold.kepczynski@gmail.com>
2023-05-26 22:46:56 +0200

    screen arrangement

--- DAW.SPA/tracks/src/app/app.component.ts
+++ DAW.SPA/tracks/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
 })
 export class AppComponent {
-  title = 'tracks';
 }
